6887ec34a16d6591b40f72978d6fb14ecddccf11
Sihil Rashmika <117860667+rashmikasihil@users.noreply.github.com>
2026-02-02 20:54:36 +0530

    models file create

---
+++ backend/models/Hotel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const hotelSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  pricePerNight: { type: Number, required: true },
+  rooms: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Hotel", hotelSchema);
